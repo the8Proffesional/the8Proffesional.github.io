122640a9e13b8bb74977657718af0d6c46f355cb
the8Proffesional <adil8adraibi@gmail.com>
2023-03-06 00:22:13 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// DOM Manipulation
+
+const title = document.getElementById('main-heading');
+const listItems = document.getElementsByClassName('list-items');
+const listItems1 = document.getElementsByTagName('li');
+
+console.log(listItems1[2]);
